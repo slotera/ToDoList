bcf4867aa6eebf5ddb43b4628cbc2b2837b8cf45
Ali Tonkaz <alitonkaz842@gmail.com>
2017-12-21 01:44:24 +0300

    Project v0.1

---
+++ Firebase_toDoList-master/Firebase_toDoList-master/app/src/main/java/com/tolist/firebase_tolist/Item.java
@@ -0,0 +1,21 @@
+package com.tolist.firebase_tolist;
+
+
+public class Item {
+
+    private String title;
+
+    public Item() {}
+
+    public Item(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
